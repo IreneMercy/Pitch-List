7b0961638deef6f71d932ffddc7e0617edf1d8ae
IreneMercy <irenemercy700@gmail.com>
2019-09-14 23:04:47 +0300

    add signup template

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app =  Flask(__name__)
 db = SQLAlchemy(app)
+loginmanager = LoginManager(app)
+loginmanager.login_view = 'auth.login'
+loginmanager.session_protection = 'strong'
 
 def create_app():
     app.config.from_object(Config)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from . import db
+from . import db,loginmanager
 from werkzeug import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(255),nullable=False,unique=True)
@@ -22,6 +23,10 @@ class User(db.Model):
 
     def check_password(self,password):
         return check_password_hash(self.password,password)
-        
+
     def __repr__(self):
         return f'User{self.username}'
+        
+    @loginmanager.user_loader
+    def user_loader(user_id):
+        return User.query.get(user_id)
