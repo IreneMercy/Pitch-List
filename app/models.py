41e4ca554f7d07462e169e8e59229731600e0f49
IreneMercy <irenemercy700@gmail.com>
2019-09-14 22:43:47 +0300

    add user class

--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
 from . import db
+from werkzeug import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),nullable=False,unique=True)
+    email = db.Column(db.String(255),nullable=True,unique=True)
+    password = db.Column(db.String(255),nullable=False)
+
+    def __repr__(self):
+        return f'User{self.username}'
